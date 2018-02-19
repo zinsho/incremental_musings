a997a7dfc3d35a185d1022de3a23674c2079b03d
Zinsho Lexagen <zinsho@gmail.com>
2018-02-19 12:43:00 -0500

    Rename to reflect extended behaviour.
    Not just tab switching but actual keybinds.

--- scripts/js/kg-keybinds.js
+++ scripts/js/kg-keybinds.js
@@ -1,5 +1,5 @@
 // update key, shift,alt,ctrl as desired but key must be capital letter for it to ensure it maps
-var tablist = [
+var keybinds = [
     {
         name: 'Bonfire',
         key: 'B',
@@ -62,21 +62,54 @@ var tablist = [
         shift: false,
         alt: false,
         control: false,
-        action: () => {$('.dialog:visible').last().hide()}
-    }
+        action: () => { $('div.dialog:visible').last().hide() }
+    },
+    {
+        name: "Send Hunters",
+        key: "h",
+        shift: false,
+        alt: false,
+        control: false,
+        action: () => { $("a:contains('Send hunters')").click() }
+    },
+    {
+        name: "Praise the Sun",
+        key: "p",
+        shift: false,
+        alt: false,
+        control: false,
+        action: () => { $("a:contains('Praise the sun')").click() }
+    },
+    {
+        name: "Observe",
+        key: "o",
+        shift: false,
+        alt: false,
+        control: false,
+        action: () => {
+            var starChart = $('#observeBtn')
+            if (starChart[0]) {
+                starChart.click()
+            }
+        }
+    },
 ]
 
-function switchTab(event) {
-    var newTab = tablist.find(x => x.key === event.key && x.switch == event.switchKey && x.alt == event.altKey && x.control == event.ctrlKey)
-    if (newTab && newTab.name != game.ui.activeTabId) {
-        if (newTab.action) {
-            newTab.action()
+function callKeybind(event) {
+    var keybind = keybinds.find(x =>
+        x.key === event.key &&
+        x.shift == event.shiftKey &&
+        x.alt == event.altKey &&
+        x.control == event.ctrlKey)
+    if (keybind && keybind.name != game.ui.activeTabId) {
+        if (keybind.action) {
+            keybind.action()
         } else {
-            console.log('Switching to: ' + newTab.name)
-            game.ui.activeTabId = newTab.name
+            console.log('Switching to: ' + keybind.name)
+            game.ui.activeTabId = keybind.name
             game.ui.render()
         }
     }
 }
 
-window.addEventListener('keyup',event => switchTab(event))
+window.addEventListener('keyup', event => callKeybind(event))
